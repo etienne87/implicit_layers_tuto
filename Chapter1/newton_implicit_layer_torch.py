727a8750dd2d990f13b8f27b30cf9c8429c6d070
etienne87 <et.perot@gmail.com>
2021-08-01 19:52:00 +0200

    adding implicit layer

---
+++ Chapter1/newton_implicit_layer_torch.py
@@ -0,0 +1,37 @@
+"""
+http://implicit-layers-tutorial.org/introduction/
+
+Newton Implicit layer
+"""
+import torch
+import torch.nn as nn
+
+
+class TanhNewtonImplicitLayer(nn.Module):
+    def __init__(self, out_features, tol = 1e-4, max_iter=50):
+        super().__init__()
+        self.linear = nn.Linear(out_features, out_features, bias=False)
+        self.tol = tol
+        self.max_iter = max_iter
+
+    def forward(self, x):
+        # Run Newton's method outside of the autograd framework
+        with torch.no_grad():
+            z = torch.tanh(x)
+            self.iterations = 0
+            while self.iterations < self.max_iter:
+                z_linear = self.linear(z) + x
+                g = z - torch.tanh(z_linear)
+                self.err = torch.norm(g)
+                if self.err < self.tol:
+                    break
+
+                # newton step
+                J = torch.eye(z.shape[1], device=x.device)[None,:,:] - (1 / torch.cosh(z_linear)**2)[:,:,None]*self.linear.weight[None,:,:]
+                z = z - torch.solve(g[:,:,None], J)[0][:,:,0]
+                self.iterations += 1
+
+        # reengage autograd and add the gradient hook
+        z = torch.tanh(self.linear(z) + x)
+        z.register_hook(lambda grad : torch.solve(grad[:,:,None], J.transpose(1,2))[0][:,:,0])
+        return z
