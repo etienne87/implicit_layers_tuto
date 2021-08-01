4118f41772cd372ca6113d58bd151e50224bf637
etienne87 <et.perot@gmail.com>
2021-08-01 19:16:48 +0200

    adding newton layer, we obtain worse accuracy

--- Chapter1/newton_layer_torch.py
+++ Chapter1/newton_layer_torch.py
@@ -1,24 +1,21 @@
 """
 http://implicit-layers-tutorial.org/introduction/
 
-Fixed-Point Iterations
+Newton Steps
 """
 import torch
 import torch.nn as nn
 
 
-class TanhFixedPointLayer(nn.Module):
+
+class TanhNewtonLayer(nn.Module):
     """
     z* = tanh(W@z* + x) : optimal z
 
-    Analogy to rnn:
-    - z is a form of hidden_state
-    - x is never-changing input
+    g(x,z) = z - tanh(W@z + x)
+    dg(x,z)/dz = I - diag(sech(W@z + x).W)
 
-
-    (A general form would be
-    z* = tanh(W_h@z* + W_x@x)
-
+    (pure gd z := dg(x,z)/dz
     """
     def __init__(self, out_features, tol=1e-4, max_iter=50):
         super().__init__()
@@ -27,21 +24,31 @@ class TanhFixedPointLayer(nn.Module):
         self.max_iter = max_iter
 
     def forward(self, x):
-        z = torch.zeros_like(x)
+        z = torch.tanh(x)
         self.err = float('inf')
         self.iterations = 0
 
         while self.err > self.tol and self.iterations < self.max_iter:
-            z_next = torch.tanh(self.linear(z) + x)
-            self.err = torch.norm(z_next-z).item()
-            self.iterations += 1
-            z = z_next
+            z_linear = self.linear(z) + x
+            g = z - torch.tanh(z_linear)
+            self.err = torch.norm(g)
 
+            if self.err <= self.tol:
+                break
+
+            # newton-step
+            J = torch.eye(z.shape[1], device=x.device)[None,:,:] - (1 / torch.cosh(z_linear)**2)[:,:,None]*self.linear.weight[None,:,:] # jacobian N,out_features,out_features
+            # what is going on here ???
+            z = z - torch.solve(g[:,:,None], J)[0][:,:,0]
+            self.iterations += 1
+
+        g = z - torch.tanh(self.linear(z) + x)
+        z[torch.norm(g,dim=1) > self.tol,:] = 0
         return z
 
 
 def random_demo():
-    layer = TanhFixedPointLayer(50)
+    layer = TanhNewtonLayer(50)
     X = torch.randn(10,50)
     Z = layer(X)
     print(f"Terminated after {layer.iterations} iterations with error {layer.err}")
